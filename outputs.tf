e0583613194fd61fb7aaf11cc575990d591dc4cf
Seunghwan Lee <cycle.lee@samsung.com>
2022-07-01 01:48:38 +0000

    modified module

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,7 @@
-output "vpc_id" {
-  value       = try(aws_vpc.this[0].id, "")
-}
-output "name_prefix" {
-  value = local.name_tag_prefix
-}
-output "vpc_cidr" {
-  value       = var.vpc_cidr
-}
-output "igw_id" {
-  value = aws_internet_gateway.this.id
+output "info" {
+  value = {
+    id     = try(aws_vpc.this.id, "")
+    cidr   = try(aws_vpc.this.cidr_block, "")
+    igw_id = try(aws_internet_gateway.this.id, "")
+  }
 }
